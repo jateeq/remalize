86ff2f934047aca47e0fca66c57a269862163150
Jawad Ateeq <jawad.ateeq@gmail.com>
2021-02-14 11:13:26 -0500

    removing dependency or argparse to make running easier, argparse too heavyweight

--- main.py
+++ main.py
@@ -1,36 +1,48 @@
 """
 
 """
+import os
+import sys
 import logging
-import argparse
 
 from typing import List, Dict
+from entity_extractor import EntityExtractor
 
 logger = logging.getLogger()
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-f', '--dream-file', help="dream text file")
-    args = parser.parse_args()
+MIN_DREAM_TEXT_CHARS = 1
 
-    dream = None
-    with open(args.dream_file, 'r') as fin:
-        dream = fin.read()
-    
-    if (dream is None) | len(dream) < 1:
-        logger.error("Invalid dream")
-        print("Invalid dream file. Exiting")
-        exit
+def main():
+    source = sys.argv[1]
+
+    input_is_not_filepath = not os.path.exists(source)
+    input_is_invalid_dream = len(source) < MIN_DREAM_TEXT_CHARS
+    if (source==None) or ((input_is_not_filepath) and (input_is_invalid_dream)):
+        raise ValueError(
+            f"Invalid source. Either input dream text greater" +
+            "than {MIN_DREAM_TEXT_CHARS} characters or provide a valid file" +
+            "path to read from.")
+
+    if not input_is_not_filepath:
+        with open(source, 'r') as fin:
+            dream = fin.read()
+        try:
+            assert len(dream) > MIN_DREAM_TEXT_CHARS
+        except AssertionError:
+            raise AssertionError(f"Dream size must be > {MIN_DREAM_TEXT_CHARS}"+
+                "for this parsing to make sense.")
+    else:
+        dream = source
 
     symbols = get_dream_symbols(dream)
     associations = {symbol:get_associations(symbol) for symbol in symbols}
     interpretation = get_interpretation(associations)
-
     print_output(dream, associations, interpretation)
 
 
 def get_dream_symbols(dream:str) -> List[str]:
-    return ['sym_1', 'sym_2', 'sym_3']
+    #return EntityExtractor().extract(dream)    
+    return ['asc_1', 'asc_2', 'asc_3']
 
 
 def get_associations(symbol:str) -> List[str]:
